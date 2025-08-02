3df36ab1dbecd475469989c1d74c0873ed9097b9
unknown <sarabyex@outlook.com>
2025-08-02 17:48:47 -0600

    correcting of pages display

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const db = require('./firebase/firebase.js');
+
 const incomeRoutes = require("./routes/income.js");
 const expensesRoutes = require("./routes/expenses.js");
 const usersRoutes = require("./routes/users.js");
@@ -10,8 +12,12 @@ const port = 3000;
 const cors = require("cors");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
-app.use(express.static("public"));
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html")); //corrected the github pages issue//
+});
 app.use(cors());
 app.use("/income", incomeRoutes);
 app.use("/expenses", expensesRoutes);
